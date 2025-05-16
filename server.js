a5c6f6c68cf350a5911b6467e3b3f8cb9626b8e3
Kevin <kevikoltraka157@gmail.com>
2025-05-16 18:12:39 +0200

    changes

--- server.js
+++ server.js
@@ -1187,7 +1187,7 @@ app.get("/health", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
